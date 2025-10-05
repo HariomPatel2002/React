c7776bf0c6384ca018cca1be257ad7be693aecc7
Hariom Patel <101473914+HariomPatel2002@users.noreply.github.com>
2025-10-05 16:55:28 +0530

    Initial push to React repo

--- routes/menuItemRoutes.js
+++ routes/menuItemRoutes.js
@@ -17,7 +17,7 @@ router.post('/', async(req,res)=>{
 });
 router.get('/', async(req,res)=>{
     try{
-        const data = await MenuItem.find();
+        const data = git add .await MenuItem.find();
         console.log(data);
         res.status(200).json(data);
 
